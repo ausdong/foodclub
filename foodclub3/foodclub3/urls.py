b648c911816c906b7c8da6d915287181905c8336
ausdong <aus.dong@gmail.com>
2014-04-15 15:52:12 -0400

    fix posts with space in names
    posts with spaces in the name should now load post page

--- foodclub3/foodclub3/urls.py
+++ foodclub3/foodclub3/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,8 +10,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'foodclub3.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^submit/', 'blog.views.submit'),
-    url(r'^(?P<name>[\w\-]+)/$', 'blog.views.post'),
-    url(r'^$', 'blog.views.index'),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^submit/', 'blog.views.submit'),
+	url(r'^(?P<name>[\w\-]+)/$', 'blog.views.post'),
+	url(r'^(?P<name>[\w|\W]+)/$', 'blog.views.post'),
+	url(r'^$', 'blog.views.index'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
