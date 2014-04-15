97e291dc5d6fbc60b96de9db6396ae1c451b6913
ausdong <aus.dong@gmail.com>
2014-04-15 16:35:56 -0400

    install comments framework

--- foodclub3/foodclub3/settings.py
+++ foodclub3/foodclub3/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.comments',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 	'blog',
 )
 
